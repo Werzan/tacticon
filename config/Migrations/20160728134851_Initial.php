5d59f12e830e049679f24412bcf2ab81c538039e
Madarász Máté <madaraszmate@ccsys.eu>
2016-07-28 16:18:00 +0200

    Initial

---
+++ config/Migrations/20160728134851_Initial.php
@@ -0,0 +1,186 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Initial extends AbstractMigration
+{
+
+    public $autoId = false;
+
+    public function change()
+    {
+        $this->table('users')
+            ->addColumn('id', 'integer', [
+                'autoIncrement' => true,
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addPrimaryKey(['id'])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('email', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('password', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addIndex(
+                [
+                    'email',
+                ],
+                ['unique' => true]
+            )
+            ->create();
+
+        $this->table('contacts')
+            ->addColumn('id', 'integer', [
+                'autoIncrement' => true,
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addPrimaryKey(['id'])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('email', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('tel', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => 11,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addIndex(
+                [
+                    'email',
+                ],
+                ['unique' => true]
+            )
+            ->addIndex(
+                [
+                    'user_id',
+                ]
+            )
+            ->create();
+
+        $this->table('groups')
+            ->addColumn('id', 'integer', [
+                'autoIncrement' => true,
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addPrimaryKey(['id'])
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('user_id', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addIndex(
+                [
+                    'user_id',
+                ]
+            )
+            ->create();
+
+        $this->table('contacts_groups')
+            ->addColumn('group_id', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addColumn('contact_id', 'integer', [
+                'default' => null,
+                'limit' => 10,
+                'null' => false,
+                'signed' => false,
+            ])
+            ->addPrimaryKey(['group_id', 'contact_id'])
+            ->addIndex(
+                [
+                    'contact_id',
+                ]
+            )
+            ->addIndex(
+                [
+                    'group_id',
+                ]
+            )
+            ->create();
+
+
+
+        $this->table('contacts')
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                [
+                    'update' => 'CASCADE',
+                    'delete' => 'CASCADE'
+                ]
+            )
+            ->update();
+
+        $this->table('groups')
+            ->addForeignKey(
+                'user_id',
+                'users',
+                'id',
+                [
+                    'update' => 'CASCADE',
+                    'delete' => 'CASCADE'
+                ]
+            )
+            ->update();
+
+        $this->table('contacts_groups')
+            ->addForeignKey(
+                'contact_id',
+                'contacts',
+                'id',
+                [
+                    'update' => 'CASCADE',
+                    'delete' => 'CASCADE'
+                ]
+            )
+            ->addForeignKey(
+                'group_id',
+                'groups',
+                'id',
+                [
+                    'update' => 'CASCADE',
+                    'delete' => 'CASCADE'
+                ]
+            )
+            ->update();
+    }
+}
