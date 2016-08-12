65efde7a765d8c8762039e3023301d2a89eff7f9
Madarász Máté <madaraszmate@ccsys.eu>
2016-08-12 15:20:25 +0200

    100%  test

--- src/View/AjaxView.php
+++ src/View/AjaxView.php
@@ -22,6 +22,8 @@ use Cake\Network\Response;
  * A view class that is used for AJAX responses.
  * Currently only switches the default layout and sets the response type -
  * which just maps to text/html by default.
+ *
+ * @codeCoverageIgnore
  */
 class AjaxView extends AppView
 {
